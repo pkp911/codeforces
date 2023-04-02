333d3f0c90d9ce3e7292470dbfc9cb0e76b1b4c6
Prashant <panprashant123@gmail.com>
2023-04-02 22:34:33 +0530

    codeforces code

--- C_Make_It_Permutation.cpp
+++ C_Make_It_Permutation.cpp
@@ -53,16 +53,12 @@ void khatam(ll n){cout<<n<<endl;}
 void khatamsab(vector<ll>v){f0(i, 0, v.size()){cout<<v[i]<<" ";}cout<<endl;}
 ll phin(ll n) {ll number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0) n /= 2;} for (ll i = 3; i <= sqrt(n); i += 2) {if (n % i == 0) {while (n % i == 0)n /= i; number = (number / i * (i - 1));}} if (n > 1)number = (number / n * (n - 1)) ; return number;} //O(sqrt(N))
 void solve(){
-    ll n, c, d;
-    cin>>n>>c>>d;
-    vector<ll>v(n);
-    aaja(v);
-    
+  khatam(0);
 }
 int32_t main()
 {
    fast;
-    ll test;
+    ll test=1;
     cin>>test;
     while (test--)
     {
